58204a76941cef5b8271879d857096f931f99602
Conner Dodge <cdodge@squareup.com>
2022-06-09 13:36:16 -0700

    Add derived count

--- src/routes/count/index.svelte
+++ src/routes/count/index.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import { derived } from 'svelte/store';
 	import Counter from './Counter.svelte';
+	import { count } from '$stores/count';
+
+	const squaredCount = derived(count, ($count: number) => Math.pow($count, 2));
 </script>
 
 <svelte:head>
@@ -12,6 +16,8 @@
     Count
   </h3>
   <Counter />
+
+	<p>Derived Count: {$squaredCount}</p>
 </section>
 
 <style>
@@ -21,5 +27,10 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
+	}
+	
+	p {
+		font-size: 18px;
+		font-weight: 600;
 	}
 </style>
