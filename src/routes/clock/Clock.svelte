6d921d2daf4ce19ae878684a26068596cac0f101
Conner Dodge <cdodge@squareup.com>
2022-06-09 10:55:09 -0700

    Add clock

---
+++ src/routes/clock/Clock.svelte
@@ -0,0 +1,42 @@
+<svelte:head>
+  <link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+</svelte:head>
+<script lang="ts">
+  import { onDestroy } from 'svelte';
+
+  let date = new Date();
+  const dateInterval = setInterval(() => {
+    date = new Date();
+  }, 1000);
+
+  $: hours = String(date.getHours()).padStart(2, '0');
+  $: minutes = String(date.getMinutes()).padStart(2, '0');
+  $: seconds = String(date.getSeconds()).padStart(2, '0');
+
+  onDestroy(() => {
+    clearInterval(dateInterval);
+  });
+</script>
+
+<div class="container">
+  <span class="clock">{hours}:{minutes}:{seconds}</span>
+</div>
+
+
+<style>
+  .container {
+    background-color: #000;
+    margin: 10% 0 0 33%;
+    max-width: 355px;
+    border-radius: 10px;
+  }
+
+  .clock {
+    color: red;
+    width: 100%;
+    margin: 0 20px;
+    font-size: 64px;
+    font-weight: 500;
+    font-family: 'Orbitron', sans-serif;
+  }
+</style>
--- src/routes/clock/index.svelte
+++ src/routes/clock/index.svelte
@@ -2,3 +2,9 @@
 	<title>Clock</title>
 	<meta name="description" content="A clock for time" />
 </svelte:head>
+
+<script lang="ts">
+	import Clock from './Clock.svelte';
+</script>
+
+<Clock />
